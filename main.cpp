263293991c0fbae4c46f9788fe52d4bc62bdfb57
duncan <duncan@duncans-MacBook-Pro.local>
2016-01-25 16:06:22 -0500

    added two basic files

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+#include "utility.h"
+//using namespace std;
+
+int main(int argc, char const *argv[]){
+	
+	printf("Hello\n");
+
+/*Pre-processing Functions*/
+	//attribute generator
+
+	//hash scheme index construction
+
+	//super-graph construction
+
+
+/*Query Time Functions*/
+	//query generator
+
+	//read graph topology
+
+	//query algorithm
+
+
+	return 0;
+}
+
+
+
