51a4f122b40516e5773cf1a705d9c5ff2baa6aff
duncan <duncan@duncans-MacBook-Pro.local>
2016-01-27 23:21:52 -0500

    doing query algo

--- QueryHandler.cpp
+++ QueryHandler.cpp
@@ -8,6 +8,12 @@ bool QueryHandler::CReachabilityQuery(vector<vector<pair<int,int> > >& topology,
 	queue<pair<int,int> > qu;
 	vector<bool> visited;
 	visited.assign(topology.size(),false);
+	unordered_map<unsigned long long,bool> satTableE,satTableV;
+
+	char vertexAttrFileName[200],edgeAttrFileName[200];
+	sprintf(vertexAttrFileName,"%s/VertexAttr.txt",attrFolderName);
+	sprintf(edgeAttrFileName,"%s/EdgeAttr.txt",attrFolderName);
+
 	while(!qu.empty()){
 		pair<int,int> h = qu.front();
 		qu.pop();
@@ -16,7 +22,7 @@ bool QueryHandler::CReachabilityQuery(vector<vector<pair<int,int> > >& topology,
 			continue;
 		visited[h.first]=true;
 
-		bool result = DFS_C(h.first,topology,vertexHashValues,edgeHashValues,q,qu,visited);
+		bool result = DFS_C(h.first,topology,vertexHashValues,edgeHashValues,q,qu,visited,satTableE,satTableV,vertexAttrFileName,edgeAttrFileName);
 //		qu.push(make_pair());
 	}
 
@@ -24,21 +30,23 @@ bool QueryHandler::CReachabilityQuery(vector<vector<pair<int,int> > >& topology,
 	return false;
 }
 
-
 bool QueryHandler::DFS_C(int cur, vector<vector<pair<int,int> > >& topology,vector<unsigned long long>& vertexHashValues,
-						vector<unsigned long long>& edgeHashValues,query& q,queue<pair<int,int> >& qu,vector<bool>& visited){
+						vector<unsigned long long>& edgeHashValues,query& q,queue<pair<int,int> >& qu,vector<bool>& visited,
+						unordered_map<unsigned long long,bool>& satTableE,unordered_map<unsigned long long,bool>& satTableV,
+						const char* vertexAttrFileName,const char* edgeAttrFileName){
 
 	for(int i=0; i<topology[cur].size(); i++){
 		if(topology[cur][i].first == q.dest)
 			return true;
 		if(visited[topology[cur][i].first]==false){
-//			if(!CheckConstraint())//check Edge constraint
-//				continue;
+			if(!CheckConstraint(topology[cur][i].second,edgeHashValues,q.edgeAttrCon,satTableE,edgeAttrFileName))//check Edge constraint
+				continue;
 //			if(!CheckConstraint())//check vertex constraint
 //				continue;
 			
 			visited[topology[cur][i].first] = true;
-			bool result = DFS_C(topology[cur][i].first,topology,vertexHashValues,edgeHashValues,q,qu,visited);
+			bool result = DFS_C(topology[cur][i].first,topology,vertexHashValues,edgeHashValues,q,qu,visited,satTableE,satTableV,
+								vertexAttrFileName,edgeAttrFileName);
 			if(result == true)
 				return true;
 		}
@@ -46,3 +54,45 @@ bool QueryHandler::DFS_C(int cur, vector<vector<pair<int,int> > >& topology,vect
 
 	return false;
 }
+
+
+
+bool QueryHandler::CheckConstraint(int id, vector<unsigned long long>& hashValues,vector<vector<int> >& con,
+									unordered_map<unsigned long long,bool>& satTable,const char* attrFileName){
+
+	unordered_map<unsigned long long,bool>::const_iterator got = satTable.find(hashValues[id]);
+	if(got!=satTable.end()){
+		return got->second; 
+	}else{
+		vector<int> attr;
+		IOAttr(id,attrFileName,attr);
+		if(CheckAttr(attr,con)){
+			satTable.insert(make_pair(hashValues[id],true));
+			return true;
+		}else{
+			satTable.insert(make_pair(hashValues[id],false));
+			return false;
+		}
+	}
+	return false;
+}
+
+void QueryHandler::IOAttr(int id,const char* attrFileName,vector<int>& attr){
+	//use fseek here!
+}
+
+bool QueryHandler::CheckAttr(vector<int>& attr,vector<vector<int> >& con){
+	for(int i=0; i<attr.size(); i++){
+		bool sat = false;
+		for(int j=0; j<con[i].size(); j++){
+			if(attr[i]==con[i][j]){
+				sat=true;
+				break;
+			}
+		}
+		if(sat == false)
+			return false;
+	}
+	return true;
+}
+
--- QueryHandler.h
+++ QueryHandler.h
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <queue>
+#include <unordered_map>
 
 #include "query.h"
 
@@ -18,7 +19,11 @@ class QueryHandler{
 
 	public:
 		bool CReachabilityQuery(vector<vector<pair<int,int> > >&,vector<unsigned long long>&,vector<unsigned long long>&,query&,const char*);
-		bool DFS_C(int,vector<vector<pair<int,int> > >&,vector<unsigned long long>&,vector<unsigned long long>&,query&,queue<pair<int,int> >&,vector<bool>&);
+		bool DFS_C(int,vector<vector<pair<int,int> > >&,vector<unsigned long long>&,vector<unsigned long long>&,query&,queue<pair<int,int> >&,
+			vector<bool>&,unordered_map<unsigned long long,bool>&,unordered_map<unsigned long long,bool>&,const char*,const char* );
+		bool CheckConstraint(int,vector<unsigned long long>&,vector<vector<int> > &,unordered_map<unsigned long long,bool>&,const char*);
+		void IOAttr(int,const char*,vector<int>&);
+		bool CheckAttr(vector<int>&,vector<vector<int> >&);
 };
 
 
